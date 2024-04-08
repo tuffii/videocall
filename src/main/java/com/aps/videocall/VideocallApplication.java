1ada9b736a2d8ee432ea9cd32e694e48101243db
tuffii <rudi1947@yandex.ru>
2024-04-08 14:47:45 +0300

    init

---
+++ src/main/java/com/aps/videocall/VideocallApplication.java
@@ -0,0 +1,14 @@
+package com.aps.videocall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class VideocallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideocallApplication.class, args);
+	}
+
+}
