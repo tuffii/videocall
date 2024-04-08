4ee265e240dd71332d8ec0faf0b541415fde63a8
tuffii <rudi1947@yandex.ru>
2024-04-08 15:01:01 +0300

    initialize simple users service

--- src/main/java/com/aps/videocall/VideocallApplication.java
+++ src/main/java/com/aps/videocall/VideocallApplication.java
@@ -4,11 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
 public class VideocallApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(VideocallApplication.class, args);
 	}
-
 }
